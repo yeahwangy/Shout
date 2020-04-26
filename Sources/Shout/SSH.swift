e720e9dfb75d380fab328ea43d89246110cbe9bc
Dmitriy Borovikov <dmitry@borovikov.name>
2020-04-26 09:27:52 +0300

    Fix bugs

--- Sources/Shout/SSH.swift
+++ Sources/Shout/SSH.swift
@@ -147,9 +147,7 @@ public class SSH {
         while dataLeft {
             switch channel.readData() {
             case .data(let data):
-                let str: String = data.withUnsafeBytes {
-                    return String(cString: $0.bindMemory(to: UInt8.self).baseAddress!)
-                }
+                let str: String = String(data: data, encoding: .utf8) ?? String(data: data, encoding: .nonLossyASCII)
                 output(str)
             case .done:
                 dataLeft = false
